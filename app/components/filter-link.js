54b335e77f1199f01bf7393537c4eb020acd4553
mbarry <m.matt.barry@gmail.com>
2017-10-04 09:24:17 -0400

    created some code for filtering individual links which isn't currently working

--- app/components/filter-link.js
+++ app/components/filter-link.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
 
 const FilterLinkComponent = Ember.Component.extend({
+  tagName: '',
+  isFiltered: Ember.computed('{param,field}', function(){
+    return this.get('field') in this.get('param');
+  }),
+  toggleFilter: Ember.computed('{param,field,data,isFiltered}', function(){
+    const beep = this.get('param');
+    const field = this.get('field');
+    const data = this.get('data');
+    if (this.get('isFiltered')){
+      let result = delete beep[field];
+      return result;
+    } else {
+      beep[field] = data;
+      return beep;
+    }
+  })
 });
 
 FilterLinkComponent.reopenClass({
