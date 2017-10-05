d9362de7f8bc9bcfa60ef28850d54c989675aa31
mbarry <m.matt.barry@gmail.com>
2017-10-05 15:58:03 -0400

    created barebones version of a filter. Up next: edit filter queryparams to be able to handle partial matches

--- app/components/advanced-search.js
+++ app/components/advanced-search.js
@@ -1,20 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  groupedFilters: {},
-  didReceiveAttrs(){
-    this._super(...arguments);
-    this.send('setGroupedFilters')
-  },
   actions: {
     applyFilter(obj){
       let output = {filter: {}};
       output.filter[obj.field] = obj.title;
       this.get('onChange')(output);
     },
-    setGroupedFilters(){
-      const pool = this.get('pool');
-      console.log(pool);
+    testAction(){
+      let output = {filter: {}};
+      output.filter[this.selectedField] = this.filter;
+      this.get('onChange')(output);
     }
   }
 });
